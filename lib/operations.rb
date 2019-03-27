df7d49b2cb7efbf46559d887393f40f2af496d48
mjm03 <mjm03@hampshire.edu>
2019-03-27 21:49:16 +0000

    Done.

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,12 +1,11 @@
 def unsafe?(speed)
-
+  if speed < 30 %% speed > 60  
+    return true 
+    else
+    return false
 end
-
-
 
 def not_safe?(speed)
+  if speed < 30 %% speed > 60 ? true : false
 	
 end
-	
-
-
